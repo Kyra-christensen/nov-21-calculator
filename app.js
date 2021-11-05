f28ee48aa5e0a1fd9c046e827f6a60760a6b6da2
Kyra Christensen <christensen.kyral@gmail.com>
2021-11-05 16:00:11 -0700

    added multiplication and division function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import functions and grab DOM elements
 import { add } from './math-utils.js';
 import { sub } from './math-utils.js';
+import { multi } from './math-utils.js';
 // initialize state
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
@@ -11,6 +12,12 @@ const subNumber1 = document.getElementById('sub-number-1');
 const subNumber2 = document.getElementById('sub-number-2');
 const subButton = document.getElementById('sub-button');
 const subAnswer = document.getElementById('sub-answer');
+
+const multiNumber1 = document.getElementById('multi-number-1');
+const multiNumber2 = document.getElementById('multi-number-2');
+const multiButton = document.getElementById('multi-button');
+const multiAnswer = document.getElementById('multi-answer');
+
 // set event listeners 
   // get user input
   // use user input to update state 
@@ -27,4 +34,11 @@ subButton.addEventListener('click', () => {
     const num2 = +subNumber2.value;
     const sum = sub(num1, num2);
     subAnswer.textContent = sum;
-})
+});
+
+multiButton.addEventListener('click', () => {
+    const num1 = +multiNumber1.value;
+    const num2 = +multiNumber2.value;
+    const sum = multi(num1, num2);
+    multiAnswer.textContent = sum;
+});
--- math-utils.js
+++ math-utils.js
@@ -2,6 +2,14 @@ export function add(num1, num2) {
     return num1 + num2;
 }
 
-export function sub(num1, num2){
+export function sub(num1, num2) {
     return num1 - num2;
+}
+
+export function multi(num1, num2) {
+    return num1 * num2;
+}
+
+export function divi(num1, num2) {
+    return num1 / num2;
 }
